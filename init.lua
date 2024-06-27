d16f28301718d406ac8122591ca92d0db751b9e8
Charles Dand <cdand@kooked.co.uk>
2024-06-28 00:26:06 +0800

    Move vim options, and begin discrete files

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
