31d712cb4a7390076c81dd5823c35f5c221c81d6
Charles Dand <cdand@kooked.co.uk>
2024-06-28 04:33:18 +0800

    Add Github copilot

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
 				null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.eslint_d,
+        -- null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.rubocop,
         -- null_ls.builtins.formatting.ansiblelint,
         -- null_ls.builtins.formatting.yamllint,
