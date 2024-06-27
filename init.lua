4ba047e2c5c8c252446123d91ef780d23a724c49
Charles Dand <cdand@kooked.co.uk>
2024-06-28 00:20:42 +0800

    Add Lazy package manager

--- init.lua
+++ init.lua
@@ -2,3 +2,22 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+-- Leave this commented for now until we need it.
+-- vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+  spec = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  },
+  checker = { enabled = true },
+})
